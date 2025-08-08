3fc9d3a41bbbceb5a3fff3589c2b0e93fb0539b3
pmarnikovic <pmarnikovic@gmail.com>
2025-08-08 00:34:55 -0400

    Update curation.py

--- curation.py
+++ curation.py
@@ -30,7 +30,7 @@ def fetch_and_parse_articles():
     feeds = {
         "wall": [
             "https://www.reuters.com/rssFeed/businessNews",
-            "https://feeds.finance.yahoo.com/rss/2.0/headline?s=yhoo&region=US&lang=en-US",
+            "https://www.cnbc.com/rss-feeds/",
             "http://www.marketwatch.com/rss/topstories",
         ],
         "main": [
